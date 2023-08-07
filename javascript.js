c81a25779014567967de283df62c2857f64874a9
Jlouisa <jonathan.louisa@gmail.com>
2023-08-07 19:34:10 +0200

    Add factory and module functions

--- javascript.js
+++ javascript.js
@@ -1,13 +1,21 @@
-const gameBoard = (function () {
+const game = (function () {
   (createPlayer = function (name, marker) {
     const playerName = name;
     const playerMarker = marker;
-    return { createPlayer };
   }),
     (binding = () => {
-      a;
+      const playBtn = document.querySelector(".btnstyle1");
     }),
-    (render = () => {
-      a;
-    });
+    (createBoard = () => {
+      let gameBoard = [];
+      for (let i = 0; i < 4; i++) {
+        gameBoard[i] = document.createElement("div");
+        gameBoard[i].classList.add("square", `row${i}`);
+        for (let j = 0; j < 4; j++) {
+          gameBoard[i] = document.createElement("div");
+          gameBoard[i].classList.add("square", `row${i}Col${j}`);
+        }
+      }
+    }),
+    (render = () => {});
 })();

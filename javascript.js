cf85eb80d270cf396baf5d7b0c7c79fc28facfff
Jlouisa <jonathan.louisa@gmail.com>
2023-08-07 19:03:10 +0200

    Setup modular design pattern

---
+++ javascript.js
@@ -0,0 +1,13 @@
+const gameBoard = (function () {
+  (createPlayer = function (name, marker) {
+    const playerName = name;
+    const playerMarker = marker;
+    return { createPlayer };
+  }),
+    (binding = () => {
+      a;
+    }),
+    (render = () => {
+      a;
+    });
+})();

2f0763d85ac2f22da3cfced769b0f326e810154d
Jlouisa <jonathan.louisa@gmail.com>
2023-08-08 02:50:16 +0200

    Add turn module

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 const game = (function () {
   let players = [];
-  console.log(players);
+  let playerOneTurn = true;
 
   //Cache DOM
   const playBtnEl = document.querySelector("#playBtn");
@@ -37,23 +37,18 @@ const game = (function () {
     const playerName = name.toLowerCase();
     const playerMarker = marker;
     players.push({ name, marker });
-    console.log(players);
+    console.log(players[0]);
+    console.log(players[1]);
   };
 
   //Creator
   function createBoard() {
     let gameBoard = [];
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < 9; i++) {
       gameBoard[i] = document.createElement("div");
       gameBoard[i].classList.add("square", `row${i}`);
       render(gameBoard[i]);
       listener(gameBoard[i]);
-      for (let j = 0; j < 2; j++) {
-        gameBoard[i][j] = document.createElement("div");
-        gameBoard[i][j].classList.add("square", `row${i}Col${j}`);
-        render(gameBoard[i][j]);
-        listener(gameBoard[i][j]);
-      }
     }
   }
 
@@ -67,9 +62,37 @@ const game = (function () {
     playBtnEl.setAttribute("style", "display: none;");
     boardEl.setAttribute("style", "display: grid;");
   }
+
+  //Turn Module
+  function playerTurns() {
+    switch (playerOneTurn) {
+      case "true": {
+        ("route 1");
+        playerOneTurn = false;
+        break;
+      }
+      case "false": {
+        ("route 2");
+        playerOneTurn = true;
+        break;
+      }
+    }
+  }
+
+  //DisplayController Module
+
+  //Disable marked grids
+
+  //ScoreBoard Module
+  //
 })();
 
-//Turn Module
-//DisplayController Module
-//ScoreBoard Module
-//
+// let marks = {
+//   row1: "O",
+//   row2: "X",
+//   row3: "X",
+//   row4: "O",
+// };
+
+// let marksX = ["row1", "row4", "row5"];
+// let marksO = ["row0", "row3", "row9"];

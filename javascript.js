40ccc09eed1e4e23ea40b1ac9413a21c60998493
Jlouisa <jonathan.louisa@gmail.com>
2023-08-09 21:52:58 +0200

    Add display scoreboard

--- javascript.js
+++ javascript.js
@@ -2,9 +2,13 @@ const game = (function () {
   let players = [];
   let gameBoard = [];
   let boardGrid = [];
+  let playerName1 = "";
+  let playerName2 = "";
   let playerOneTurn = true;
 
   //Cache DOM
+  const mainEl = document.querySelector(".main");
+  const display = document.querySelector(".display");
   const playBtnEl = document.querySelector("#playBtn");
   const boardEl = document.querySelector(".board");
   const formsEl = document.querySelector(".getForms");
@@ -13,41 +17,37 @@ const game = (function () {
   const player2 = document.querySelector("#player2");
   const resetBtn = document.querySelector("#reset");
   const cpuCheck = document.querySelector("#CPU");
+  const scoreBoard = document.querySelector(".playerNames");
+  const playerOne = document.createElement("div");
+  const playerTwo = document.createElement("div");
+  const playerOneScore = document.createElement("div");
+  const playerTwoScore = document.createElement("div");
   const markerX = "X";
   const markerO = "O";
 
   console.log(cpuCheck.checked);
 
-  //CPU Toggle
-  function cpuToggle() {
-    if (cpuCheck.checked == true) {
-      player2.setAttribute("style", "visibility: hidden;");
-      player2.value = "";
-    } else {
-      player2.setAttribute("style", "visibility: visible;");
-    }
+  //Creator
+  function createDisplayPlayers() {
+    playerOne.classList.add(".playerOne");
+    playerTwo.classList.add(".playerTwo");
+    scoreBoard.appendChild(playerOne);
+    scoreBoard.appendChild(playerTwo);
+    scoreBoard.insertBefore(playerOneScore, playerTwo);
+    scoreBoard.appendChild(playerTwoScore);
+    playerOneScore.textContent = "0";
+    playerTwoScore.textContent = "0";
+    displayScores();
   }
 
-  //Verifying Player names
-  function verifyNames() {
-    let playerName1 = playerName1El.value;
-    let playerName2 = playerName2El.value;
-
-    if (playerName1 == "") {
-      playerName1 = "Player 1";
+  function createBoard() {
+    for (let i = 0; i < 9; i++) {
+      boardGrid[i] = document.createElement("div");
+      boardEl.appendChild(boardGrid[i]);
+      Namer(boardGrid[i], [i]);
+      gameBoard.push({ grid: boardGrid[i], marker: "" });
     }
-    if (playerName2 == "") {
-      playerName2 = "Player 2";
-    }
-    createPlayerNames(playerName1, playerName2);
-  }
-
-  //Setup Players
-  function createPlayerNames(player1, player2) {
-    createPlayer(player1, markerX);
-    createPlayer(player2, markerO);
-    createBoard();
-    hider();
+    listener();
   }
 
   //Hide Forms after game start
@@ -55,6 +55,7 @@ const game = (function () {
     formsEl.setAttribute("style", "display: none;");
     playBtnEl.setAttribute("style", "display: none;");
     boardEl.setAttribute("style", "display: grid;");
+    display.setAttribute("style", "display: flex;");
   }
 
   //Listener Module
@@ -73,36 +74,67 @@ const game = (function () {
     });
   }
 
-  //Factory for Players
-  createPlayer = function (name, marker) {
-    // const playerName = name;
-    // const playerMarker = marker;
-    players.push({ name, marker });
-    console.log(players);
-  };
-
-  //Creator
-  function createBoard() {
-    for (let i = 0; i < 9; i++) {
-      boardGrid[i] = document.createElement("div");
-      boardEl.appendChild(boardGrid[i]);
-      Namer(boardGrid[i], [i]);
-      gameBoard.push({ grid: boardGrid[i], marker: "" });
-    }
-    listener();
-  }
-
-  //Name Class
-  function Namer(name, i) {
-    name.classList.add("square", `row${i}`);
-  }
-
   //Renderer
   function render() {
     gameBoard.forEach((n) => {
       n.grid.textContent = n.marker;
     });
-    console.log(gameBoard);
+    console.log("test");
+  }
+  function displayScores() {
+    console.log(playerOneScore);
+    console.log(playerTwoScore);
+    playerOneScore.textContent = "0";
+    playerTwoScore.textContent = "0";
+  }
+  function displayPlayers(n1, n2) {
+    playerOne.textContent = n1;
+    playerTwo.textContent = n2;
+  }
+
+  //Factory for Players
+  createPlayer = function (name, marker) {
+    players.push({ name, marker });
+  };
+
+  //CPU Toggle
+  function cpuToggle() {
+    if (cpuCheck.checked == true) {
+      player2.setAttribute("style", "visibility: hidden;");
+      player2.value = "";
+    } else {
+      player2.setAttribute("style", "visibility: visible;");
+    }
+  }
+
+  //Verifying Player names
+  function verifyNames() {
+    playerName1 = playerName1El.value;
+    playerName2 = playerName2El.value;
+
+    if (playerName1 == "") {
+      playerName1 = "Player 1";
+    }
+    if (playerName2 == "") {
+      playerName2 = "Player 2";
+    }
+    createPlayerNames(playerName1, playerName2);
+    createDisplayPlayers();
+    displayPlayers(playerName1, playerName2);
+    displayScores();
+  }
+
+  //Setup Players
+  function createPlayerNames(player1, player2) {
+    createPlayer(player1, markerX);
+    createPlayer(player2, markerO);
+    createBoard();
+    hider();
+  }
+
+  //Name Class
+  function Namer(name, i) {
+    name.classList.add("square", `row${i}`);
   }
 
   //DisplayController Player One
@@ -137,6 +169,9 @@ const game = (function () {
         break;
     }
   }
+  displayScores();
+  displayScores();
+  displayScores();
 
   //Reset Module
   function resetFunc() {
